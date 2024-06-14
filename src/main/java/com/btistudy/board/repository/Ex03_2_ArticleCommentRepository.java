6b0b2198fb494bd3e3720868d686b8ffa0768f32
jaeyoungchoi99 <166673590+jaeyoungchoi99@users.noreply.github.com>
2024-06-14 15:52:55 +0900

    #1 - data rest 를 위한 레포지토리 (비어있음) 만듬
    .

---
+++ src/main/java/com/btistudy/board/repository/Ex03_2_ArticleCommentRepository.java
@@ -0,0 +1,10 @@
+package com.btistudy.board.repository;
+
+import com.btistudy.board.domain.ArticleComment;
+import com.btistudy.board.domain.Ex01_2_ArticleComment_엔티티로_등록;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface Ex03_2_ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
